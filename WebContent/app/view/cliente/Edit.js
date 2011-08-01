ac3a23ecaf0194d0c29270f626f82420ae0095e2
Rafael Reuber <psico.indie@gmail.com>
2011-07-31 21:39:55 -0300

    Reformulado formulário de cadastro de clientes.

--- WebContent/app/view/cliente/Edit.js
+++ WebContent/app/view/cliente/Edit.js
@@ -5,21 +5,22 @@ Ext.define('Drover.view.cliente.Edit',{
 		
 		title: 'Edição de Clientes',
 		layout: 'fit',
-		width: 730,
+		width: 700,
 		autoShow: true,
 		
 		requires: ['Ext.form.Panel',
 		           		  'Ext.form.field.ComboBox'
 		  ],
 		
-		initComponent: function (){
-			
+		initComponent: function () {		
 			this.items = [{
 					    xtype: 'form',
 				        frame:true,
 				        bodyStyle:'padding:5px 5px 0',
 				        fieldDefaults: {
-				        	labelWidth: 63
+				            labelAlign: 'top',
+				            msgTarget: 'side',
+				        	margins: '0 6 0 0'
 				        },
 				        
 				        items :  [{
@@ -31,97 +32,131 @@ Ext.define('Drover.view.cliente.Edit',{
 				        				fieldLabel: 'Nome',
 				        				xtype: 'textfield',
 				        				allowBlank: false,
-				        				flex: 2,
-				        				width: 380
+				        				flex: 6,
 				        		},{
-				        				name: 'cpfCnpj',
-				        				fieldLabel: 'CPF',
-				        				xtype: 'textfield',
-				        				allowBlank: false,
-				        				flex: 1,
-				        				width: 150,
-				        				margins: '0 0 0 6',
-				        				labelWidth: 27
-				        		}]
-				        },{// Logradouro e Número
-				        	xtype: 'container',
-				        	layout: 'hbox',
-				        	items: [{
-				        			    name: 'logradouro',
-				        				fieldLabel: 'Logradouro',
-				        				xtype: 'textfield',
-				        				allowBlank: false,
-				        				flex: 2
-				        	},{
-				        			   name: 'numeroCasa',
-				        			   fieldLabel: 'Nº',
-				        			   margins: '0 0 0 6',
-				        			   xtype: 'numberfield',
-				        			   allowBlank: false,
-				        			   labelWidth: 27,
-				        			   width: 50,
-				        			   flex: 1
-				        	}]
+			        				name:'dataNascimento',
+			        				fieldLabel: 'Data de Nascimento',
+			        				xtype: 'datefield',
+			        				flex: 2,
+			        				format: 'd/m/Y',
+					        	}]
 				        },{
 				        	xtype: 'container',
 				        	layout: 'hbox',
 				        	items:[{
-				        				name:'rg',
-				        				fieldLabel: 'RG',
-				        				fieldWidth: 12,
-				        				maxLength: 13,
-				        				xtype: 'textfield',
-				        				flex: 1
-				        	},{
+		        				name: 'cpfCnpj',
+		        				fieldLabel: 'CPF',
+		        				xtype: 'textfield',
+		        				allowBlank: false,
+		        				flex: 2,
+				          },{
+		        				name:'rg',
+		        				fieldLabel: 'RG',
+		        				maxLength: 13,
+		        				xtype: 'textfield',
+		        				flex: 3
+				          },{
+				        	  name:'orgaoexpedidor',
+				        	  xtype: 'textfield',
+				        	  fieldLabel: 'Orgão Expedidor',
+				        	  flex: 3
+				          },{
 		        				name:'dataExpedicao',
 		        				fieldLabel: 'Data de Expedição',
 		        				xtype: 'datefield',
 		        				flex: 3,
 		        				labelWidth: 110,
 		        				format: 'd/m/Y',
-		        				margins: '0 0 0 6',
-				        	},{
-		        				name:'dataNascimento',
-		        				fieldLabel: 'Data de Nascimento',
-		        				xtype: 'datefield',
-		        				flex: 3,
-		        				labelWidth: 110,
-		        				format: 'd/m/Y',
-		        				margins: '0 0 0 6',
 				        	}]
-				        },{ // Bairro Cidade e UF
-				        	xtype: 'container',
-				        	layout: 'hbox',
-				        	items: [{
+				        },{
+				        	xtype:'tabpanel', // TabPanel
+				        	plain:true,
+				        	activeTab: 0,
+				            height:200,
+				            defaults:{bodyStyle:'padding:10px'},
+				            items:[{ //Aba Endereço
+				            	title:'Endereço',
+				            	items:[{// Logradouro e Número
+						        	xtype: 'container',
+						        	layout: 'hbox',
+						        	items: [{
+						        			    name: 'logradouro',
+						        				fieldLabel: 'Logradouro',
+						        				xtype: 'textfield',
+						        				allowBlank: false,
+						        				flex: 6
+						        	},{
+					        			   name: 'numeroCasa',
+					        			   fieldLabel: 'Nº',
+					        			   xtype: 'textfield',
+					        			   allowBlank: false,
+					        			   flex: 1
+						        	}]
+						          },{ //Cep e complemento
+							        	xtype: 'container',
+							        	layout:'hbox',
+							        	items:[{
+						        			   name: 'cep',
+						        			   fieldLabel: 'CEP',
+						        			   xtype: 'textfield',
+						        			   flex: 1
+							        	},{
+							        		   name: 'complemento',
+						        			   fieldLabel: 'Complemento',
+						        			   xtype: 'textfield',
+						        			   flex: 4
+							        	}]
+							        		
+							        },{ // Bairro Cidade e UF
+						        	xtype: 'container',
+						        	layout: 'hbox',
+						        	items: [{
 				        		        name: 'bairro',
 				        		        fieldLabel: 'Bairro',
 				        		        xtype: 'textfield',
-				        		        flex: 3
-				        	},{
-				        		name: 'cidade_id',
-				        		fieldLabel: 'Cidade',
-				        		xtype: 'combobox',
-				        		store: 'Cidades',
-				        		displayField: 'nome',
-				        		valueField: 'id',
-				        		margins: '0 0 0 6',
-				        		labelWidth: 40,
-				        		id: 'cidade-combobox',
-				        		flex: 2
-				        	},{
-				        		name: 'estado',
-				        		fieldLabel: 'UF',
-				        		width: 20,
-				        		xtype: 'combobox',
-				        		store: 'Estados',
-				        		displayField: 'sigla',
-				          		action: 'selectEstadoAction',
-				        		valueField: 'id',
-				        		labelWidth: 27,
-				        		margins: '0 0 0 6',
-				        		id: 'estado-combobox',
-				        	    flex: 1
-				        	}]
+				        		        flex: 4
+						        	},{
+						        		name: 'estado',
+						        		fieldLabel: 'UF',
+						        		width: 20,
+						        		xtype: 'combobox',
+						        		store: 'Estados',
+						        		allowBlank: false,
+						        		displayField: 'sigla',
+						          		action: 'selectEstadoAction',
+						        		valueField: 'id',
+						        		id: 'estado-combobox',
+						        	    flex: 1
+						        	},{
+						        		name: 'cidade_id',
+						        		fieldLabel: 'Cidade',
+						        		xtype: 'combobox',
+						        		store: 'Cidades',
+						        		allowBlank: false,
+						        		displayField: 'nome',
+						        		valueField: 'id',
+						        		disabled: true,
+						        		id: 'cidade-combobox',
+						        		flex: 4
+						        	}]
+						        }]
+				            },{
+				            	title:'Telefones',
+				            	items: [{
+				                    fieldLabel: 'Residencial',
+				                    name: 'home',
+				                	xtype: 'textfield',
+				                    value: '(888) 555-1212'
+				                },{
+				                    fieldLabel: 'Comercial',
+				                	xtype: 'textfield',
+				                    name: 'business'
+				                },{
+				                    fieldLabel: 'Celular',
+				                	xtype: 'textfield',
+				                    name: 'mobile'
+				                }]
+				            }]
 				        }]
 				     
 			}];
@@ -132,8 +167,9 @@ Ext.define('Drover.view.cliente.Edit',{
 	        },{
 	        	text: 'Cancel',
 	        	scope: this,
-	           handler: this.close
+	            handler: this.close
 	       }];
+			
 			
 			this.callParent(arguments);
 		}
